95454c40d7cb51a5edf6d206f98128e5b95f9eae
masterdrue <masterdrue@gmail.com>
2021-02-16 20:27:35 -0500

    deployment

---
+++ build/precache-manifest.d2ea0f23085414112a63be7323671a04.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "407d382a2bb8aa79b67cfd85caa9a5b8",
+    "url": "/covidcommand/index.html"
+  },
+  {
+    "revision": "26580cd10e20cacf531d",
+    "url": "/covidcommand/static/css/2.307a1fe8.chunk.css"
+  },
+  {
+    "revision": "666cd26f4bc6491f8aa7",
+    "url": "/covidcommand/static/css/main.aef9ef48.chunk.css"
+  },
+  {
+    "revision": "26580cd10e20cacf531d",
+    "url": "/covidcommand/static/js/2.3fe718f4.chunk.js"
+  },
+  {
+    "revision": "3a5359d431b8190dc2f36e4703f39768",
+    "url": "/covidcommand/static/js/2.3fe718f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "666cd26f4bc6491f8aa7",
+    "url": "/covidcommand/static/js/main.56ec5ecf.chunk.js"
+  },
+  {
+    "revision": "16587d7c54cbd4b3b3b8",
+    "url": "/covidcommand/static/js/runtime-main.8510d368.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/covidcommand/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/covidcommand/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/covidcommand/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/covidcommand/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/covidcommand/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -48,12 +48,12 @@ class Header extends Component {
             <React.Fragment>
                 <Navbar dark sticky="top" expand="md">
                     <div className="container">
-                        <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo-trans-30.png" height="30" width="30" /></NavbarBrand>
+                        <NavbarBrand className="mr-auto" href="/"><img src="assets/images/logo-30.gif" height="30" width="30" alt="logo" /></NavbarBrand>
                         <NavbarToggler onClick={this.toggleNav} />
                         <Collapse isOpen={this.state.isNavOpen} navbar>
                             <Nav navbar>
                                 <NavItem>
-                                    <NavLink className="nav-link" to="/home">
+                                    <NavLink className="nav-link" to="/">
                                         <i className="fa fa-home fa-lg" /> Home
                                     </NavLink>
                                 </NavItem>
